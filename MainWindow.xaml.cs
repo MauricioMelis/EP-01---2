aea84ba706b34fef581c86d52053df0ec3230cae
10047775 <mauriciosantiago.melis@uvic.cat>
2024-09-23 16:10:23 +0200

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+
+namespace EP_01___2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CanviarTemaClar(object sender, RoutedEventArgs e)
+        {
+            var theme = new ResourceDictionary();
+            theme.Source = new Uri("pack://application:,,,/Themes/TemaClar.xaml");
+
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(theme);
+
+        }
+            private void CanviarTemaFosc(object sender, RoutedEventArgs e)
+        {
+            var theme = new ResourceDictionary();
+            theme.Source = new Uri("pack://application:,,,/Themes/TemaFosc.xaml");
+
+            Application.Current.Resources.MergedDictionaries.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(theme);
+
+        }
+
+    }
+}
